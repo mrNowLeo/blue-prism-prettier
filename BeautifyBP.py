2b8f105e848210269f1560d460121931b81795ab
LKarachevtsev <LKarachevtsev@ssc.beeline.ru>
2022-03-25 18:26:02 +0300

    update display position

--- BeautifyBP.py
+++ BeautifyBP.py
@@ -6,7 +6,7 @@ from Font import Font
 tree = etree.parse('ObjOld.xml')
 root = tree.getroot()
 
-def getChildArray(element, tag):
+def get_child_array(element, tag):
     arr = []
     for child in element:
         if child.tag == tag:
@@ -14,55 +14,73 @@ def getChildArray(element, tag):
                 arr.append([child_inputs.get('stage'), child_inputs.get('narrative')])
     return arr
 
-inputs = []
-outputs = []
-min_x = 1000
-process_info_x = 0
-process_info_y = 0
-subsheet_id = ""
+def beautify_BP_code():
+    inputs = []
+    outputs = []
+    min_x = 1000
+    process_info_x = 0
+    process_info_y = 0
+    subsheet_id = ""
 
-for event, elem in etree.iterparse("ObjOld.xml"):
+    for event, elem in etree.iterparse("ObjOld.xml"):
 
-    if elem.tag == 'subsheetid':
-        subsheet_id = elem.text
-    # Получение списка входящих параметров
-    if elem.get('name') == "Start":
-        inputs = getChildArray(elem, "inputs")
-    # Получение списка исходящих параметров
-    if elem.get('name') == "End":
-        outputs = getChildArray(elem, "outputs")
-    # Получение Х координаты Описания процесса, для правильного позиционирования блоков с переменными
-    if elem.get('type') == "ProcessInfo" or elem.get('type') == "SubSheetInfo":
-        for item in elem:
-            if item.tag == 'display':
-                process_info_x = int(item.get('x'))
-                process_info_y = int(item.get('y'))
-    # Получение Х координаты самого левого элемента, чтобы относительно его позиционировать Описание процесса и переменные
-    if elem.tag == "stage":
-        for item in elem:
-            if (item.tag == 'display' and int(item.get('x')) > min_x and elem.get('type') != "ProcessInfo"):
-                min_x = int(item.get('x'))
-
-for elem in root:
-    for i in range(len(inputs)):
-        if elem.get('name') in inputs[i]:
+        if elem.tag == 'subsheetid':
+            subsheet_id = elem.text
+        # Получение списка входящих параметров
+        if elem.get('name') == "Start":
+            inputs = get_child_array(elem, "inputs")
+        # Получение списка исходящих параметров
+        if elem.get('name') == "End":
+            outputs = get_child_array(elem, "outputs")
+        # Получение Х координаты Описания процесса, для правильного позиционирования блоков с переменными
+        if elem.get('type') == "ProcessInfo" or elem.get('type') == "SubSheetInfo":
             for item in elem:
                 if item.tag == 'display':
-                    item.set('x', str(process_info_x - 255))
-                    item.set('y', str(process_info_y + i * 30 + 90))
-            root.append(Stage(inputs[i][0] + '_Note', 'Note', inputs[i][1], subsheet_id, '', Display(str(process_info_x - 165), str(process_info_y + i * 30 + 90), '90', '30')))
-    for i in range(len(outputs)):
-        if elem.get('name') in outputs[i]:
+                    process_info_x = int(item.get('x'))
+                    process_info_y = int(item.get('y'))
+        # Получение Х координаты самого левого элемента, чтобы относительно его позиционировать Описание процесса и переменные
+        if elem.tag == "stage":
             for item in elem:
-                if item.tag == 'display':
-                    item.set('x', str(process_info_x - 45))
-                    item.set('y', str(process_info_y + i * 30 + 90))
-            root.append(Stage(outputs[i][0] + '_Note', 'Note', outputs[i][1], subsheet_id, '', Display(str(process_info_x + 45), str(process_info_y + i * 30 + 90), '90', '30')))
+                if (item.tag == 'display' and int(item.get('x')) > min_x and elem.get('type') != "ProcessInfo"):
+                    min_x = int(item.get('x'))
 
+    for elem in root:
+        for i in range(len(inputs)):
+            if elem.get('name') in inputs[i]:
+                for item in elem:
+                    if item.tag == 'display':
+                        item.set('x', str(process_info_x - 255))
+                        item.set('y', str(process_info_y + i * 30 + 90))
+                root.append(Stage(
+                    name = inputs[i][0] + '_Note', 
+                    type = 'Note', 
+                    narrative = inputs[i][1], 
+                    subsheet = subsheet_id,
+                    display = Display(
+                        x = str(process_info_x - 165), 
+                        y = str(process_info_y + i * 30 + 90), 
+                        w = '90', 
+                        h = '30')
+                    ).get_XML()
+                )
+         
+        for i in range(len(outputs)):
+            if elem.get('name') in outputs[i]:
+                for item in elem:
+                    if item.tag == 'display':
+                        item.set('x', str(process_info_x - 45))
+                        item.set('y', str(process_info_y + i * 30 + 90))
+                root.append(Stage(outputs[i][0] + '_Note', 'Note', outputs[i][1], subsheet_id, '', Display(str(process_info_x + 45), str(process_info_y + i * 30 + 90), '90', '30')).get_XML())
+            
+    if len(inputs) > 0:
+        # Создание блока для входящих параметров
+        root.append(Stage('Input', 'Block', '', subsheet_id, 'true', Display(str(process_info_x - 300), str(process_info_y + 60), '195', str((len(inputs) + 1) * 30)), Font(color='008000')).get_XML())
 
-# Создание блока для входящих параметров
-root.append(Stage('Input', 'Block', '', subsheet_id, 'true', Display(str(process_info_x - 300), str(process_info_y + 60), '195', str(len(inputs) * 45 - 15)), Font(color='008000')))
-# Создание блока для исходящих параметров
-root.append(Stage('Output', 'Block', '', subsheet_id, 'true', Display(str(process_info_x - 90), str(process_info_y + 60), '195', str(len(outputs) * 45 - 15)), Font(color='FF9900')))
+    if len(outputs) > 0:
+        # Создание блока для исходящих параметров
+        root.append(Stage('Output', 'Block', '', subsheet_id, 'true', Display(str(process_info_x - 90), str(process_info_y + 60), '195', str((len(outputs) + 1) * 30)), Font(color='FF9900')).get_XML())
 
-tree.write('ObjNew.xml')
+    tree.write('ObjNew.xml')
+
+if __name__ == '__main__':
+    beautify_BP_code()
--- Stage.py
+++ Stage.py
@@ -6,7 +6,7 @@ import uuid
 class Stage:
     """docstring"""
     
-    def __init__(self, name, type, narrative="", subsheet="", loginhibit="", display=Display(), font=Font()):
+    def __init__(self, name, type, narrative="", subsheet="", loginhibit="", display=Display(x=0, y=0, w=60, h=30), font=Font()):
         """Constructor"""
         self.stage_id = str(uuid.uuid4())
         self.name = name
@@ -18,26 +18,26 @@ class Stage:
         self.font = font
     
     def get_XML(self):
-        stageXMLItem= etree.Element('stage')
-        stageXMLItem.set('stageid', self.stage_id)
-        stageXMLItem.set('name', self.name)
-        stageXMLItem.set('type', self.type)
+        stage_XML_item= etree.Element('stage')
+        stage_XML_item.set('stageid', self.stage_id)
+        stage_XML_item.set('name', self.name)
+        stage_XML_item.set('type', self.type)
 
         if self.narrative is not None and self.narrative != '':
-            narrative_stageXMLItem = etree.Element('narrative')
-            narrative_stageXMLItem.text = self.narrative
-            stageXMLItem.append(narrative_stageXMLItem)
+            narrative_stage_XML_item = etree.Element('narrative')
+            narrative_stage_XML_item.text = self.narrative
+            stage_XML_item.append(narrative_stage_XML_item)
         if self.subsheet is not None and self.subsheet != '':
-            subsheetid_stageXMLItem = etree.Element('subsheetid')
-            subsheetid_stageXMLItem.text = self.subsheet
-            stageXMLItem.append(subsheetid_stageXMLItem)
+            subsheetid_stage_XML_item = etree.Element('subsheetid')
+            subsheetid_stage_XML_item.text = self.subsheet
+            stage_XML_item.append(subsheetid_stage_XML_item)
 
         if self.loginhibit is not None and self.loginhibit != '':
-            loginhibit_stageXMLItem = etree.Element('loginhibit')
-            loginhibit_stageXMLItem.set('onsuccess', self.loginhibit)
-            stageXMLItem.append(loginhibit_stageXMLItem)
+            loginhibit_stage_XML_item = etree.Element('loginhibit')
+            loginhibit_stage_XML_item.set('onsuccess', self.loginhibit)
+            stage_XML_item.append(loginhibit_stage_XML_item)
 
-        stageXMLItem.append(self.display.get_XML())
-        stageXMLItem.append(self.font.get_XML)
+        stage_XML_item.append(self.display.get_XML())
+        stage_XML_item.append(self.font.get_XML())
 
-        return stageXMLItem
+        return stage_XML_item
